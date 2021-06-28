c2048faf35f60dc369d19a141397ca0d72ceb665
EMRE <EMRE@DESKTOP-U4IGUBS>
2021-06-28 18:12:36 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //Generic Constraint
+    //IEntity:IEntity or IEntity implement
+    //new():Used for class only
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        //Used For filter
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);       
+    }
+}
